ef5bd6f948f4566f4d33d17074a8c6c1d112fe07
JuanpeDeveloper <juanpe.developer@gmail.com>
2024-08-18 01:12:20 -0300

    carrito correcciones

--- login.php
+++ login.php
@@ -9,19 +9,19 @@
 <html>
   <head>
     <title>Logeo</title>
-    <link rel="stylesheet" type="text/css" href="../global.css">
-    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="stylesheet" type="text/css" href="styles/global.css">
+    <link rel="stylesheet" type="text/css" href="styles/login.css">
   </head>
   <body>
     <section class="container">
-      <form method="post" enctype="multipart/form-data" action="comprobarlogin.php" class="form">
+      <form method="post" enctype="multipart/form-data" action="scripts/comprobarlogin.php" class="form">
         <h1>Inicio de sesión</h1>
         <label>Email</label>
         <input name="email" id="email" type="email" required placeholder="Ingrese su email...">
         <label>Contraseña</label>
         <input name="contraseña" id="contraseña" type="password" required placeholder="Ingrese su contraseña...">
         <button type="submit">Iniciar sesión</button>
-        <p>¿No tienes una cuenta aun? <a href="../register/register.php">¡Registrate!</a></p>
+        <p>¿No tienes una cuenta aun? <a href="register.php">¡Registrate!</a></p>
       </form>
     </section>
       <?php 
